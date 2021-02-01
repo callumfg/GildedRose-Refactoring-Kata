133c0ce15d010a23f91bd50dd3acc72a55bb022b
Callum Griffiths <Callum.Griffiths@softwire.com>
2021-02-01 14:41:17 +0000

    master test working

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -3,10 +3,31 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
-    });
+    it('master test', function () {
+        //Arange
+  const items = [
+    new Item("+5 Dexterity Vest", 10, 20), //
+    new Item("Aged Brie", 2, 0), //
+    new Item("Elixir of the Mongoose", 5, 7), //
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+    // this conjured item does not work properly yet
+    // new Item("Conjured Mana Cake", 3, 6)];
+  ]
 
+
+  //Act
+  const gildedRose = new GildedRose(items);
+
+  const updatedItems = gildedRose.updateQuality();
+
+  //Assert
+
+  expect(updatedItems).to.deep.include(new Item("Aged Brie", 1, 1)); 
+    })
+ 
 });
+
--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -1,4 +1,6 @@
-import { Item, GildedRose } from '../app/gilded-rose';
+// import { Item, GildedRose } from '../app/gilded-rose';
+const Item = require('../app/gilded-rose').Item;
+const GildedRose = require('../app/gilded-rose').GildedRose;
 
 const items = [
     new Item("+5 Dexterity Vest", 10, 20), //
@@ -12,9 +14,19 @@ const items = [
     // this conjured item does not work properly yet
     new Item("Conjured Mana Cake", 3, 6)];
 
+// const items = [
+//     new Item("Farron Greatsword", 1, 20),
+//     new Item("Silver Knight Armour", 3, 2),
+//     new Item("Aged Brie", 2, 2),
+//     new Item("Estus Shard", 5, 54),
+//     new Item("Sulfuras", 4, 80),
+//     new Item("Sulfuras", -1, 80),
+//     new Item("Backstage pass", 7, 15)
+// ];
+
 
 const gildedRose = new GildedRose(items);
-var days: number = 2;
+var days: number = 4;
 for (let i = 0; i < days; i++) {
     console.log("-------- day " + i + " --------");
     console.log("name, sellIn, quality");
@@ -25,3 +37,27 @@ for (let i = 0; i < days; i++) {
     console.log();
     gildedRose.updateQuality();
 }
+
+// -------- day 0 --------
+// name, sellIn, quality
+// +5 Dexterity Vest 10 20
+// Aged Brie 2 0
+// Elixir of the Mongoose 5 7
+// Sulfuras, Hand of Ragnaros 0 80
+// Sulfuras, Hand of Ragnaros -1 80
+// Backstage passes to a TAFKAL80ETC concert 15 20
+// Backstage passes to a TAFKAL80ETC concert 10 49
+// Backstage passes to a TAFKAL80ETC concert 5 49
+// Conjured Mana Cake 3 6
+
+// -------- day 1 --------
+// name, sellIn, quality
+// +5 Dexterity Vest 9 19
+// Aged Brie 1 1
+// Elixir of the Mongoose 4 6
+// Sulfuras, Hand of Ragnaros 0 80
+// Sulfuras, Hand of Ragnaros -1 80
+// Backstage passes to a TAFKAL80ETC concert 14 21
+// Backstage passes to a TAFKAL80ETC concert 9 50
+// Backstage passes to a TAFKAL80ETC concert 4 50
+// Conjured Mana Cake 2 5
